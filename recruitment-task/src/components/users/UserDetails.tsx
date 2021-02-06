5bda4457df291357be102ab5a9aeaa32d42c8ce1
Rafał Chojnowski <chojnowskigr@gmail.com>
2021-02-06 21:09:07 +0100

    feat: details in progress

--- recruitment-task/src/components/users/UserDetails.tsx
+++ recruitment-task/src/components/users/UserDetails.tsx
@@ -1,7 +1,26 @@
+import { useQuery } from '@apollo/client';
+import Skeleton from 'react-loading-skeleton';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { Post } from '../../interfaces/Post';
+import { UserDetails as UserDetailsInterface } from '../../interfaces/User';
+import { GET_USER_BY_ID } from './Queries';
 
+const DetailsHeader = styled.h1``;
+const Container = styled.div``;
 export const UserDetails: React.FC = () => {
 	const { id } = useParams<{ id: string }>();
-	console.log(id);
-	return <> </>;
+	const { data, loading, error } = useQuery<{ user: UserDetailsInterface }>(GET_USER_BY_ID, {
+		variables: { id },
+	});
+	return (
+		<>
+			<Container>
+				cofajka
+				<DetailsHeader>{data?.user.name || <Skeleton />}</DetailsHeader>
+				dodawajka
+				{data?.user.posts?.data.map((p: Post) => p.title)}
+			</Container>
+		</>
+	);
 };
