722a6124d8aa386e9e866ac0e4ebe05b7afc1bfa
Rafał Chojnowski <chojnowskigr@gmail.com>
2021-02-07 11:02:45 +0100

    feat: UserDetails header loading skeleton add

--- recruitment-task/src/components/users/UserDetails.tsx
+++ recruitment-task/src/components/users/UserDetails.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@apollo/client';
 import Skeleton from 'react-loading-skeleton';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { PostListItem as PostListItemInterface } from '../../interfaces/Post';
 import { UserDetails as UserDetailsInterface } from '../../interfaces/User';
@@ -8,13 +8,18 @@ import { PostListItem } from '../posts/PostListItem';
 import { GET_USER_BY_ID } from './Queries';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
 import { ImPlus } from 'react-icons/im';
+import { useCallback } from 'react';
 
 const Container = styled.div``;
 const DetailsHeader = styled.h1`
 	margin: 0 0 0 0;
+	height: 100%;
+	width: 100%;
+	text-align: center;
 `;
 const HeaderContainer = styled.div`
 	display: flex;
+	width: 100%;
 	justify-content: space-between;
 `;
 
@@ -23,13 +28,21 @@ export const UserDetails: React.FC = () => {
 	const { data, loading, error } = useQuery<{ user: UserDetailsInterface }>(GET_USER_BY_ID, {
 		variables: { id },
 	});
+	const history = useHistory();
+	const backRedirect = useCallback(() => {
+		history.goBack();
+	}, [history]);
+	const createPost = useCallback(() => {
+		console.log('create');
+	}, []);
+
 	return (
 		<>
 			<Container>
 				<HeaderContainer>
-					<AiOutlineArrowLeft style={{ fontSize: '30px' }} />
+					<AiOutlineArrowLeft style={{ fontSize: '30px' }} onClick={backRedirect} />
 					<DetailsHeader>{data?.user.name || <Skeleton />}</DetailsHeader>
-					<ImPlus style={{ fontSize: '30px' }} />
+					<ImPlus style={{ fontSize: '30px' }} onClick={createPost} />
 				</HeaderContainer>
 				{loading
 					? [1, 2, 3, 4, 5, 6, 7, 8].map((i) => <PostListItem key={i} />)
