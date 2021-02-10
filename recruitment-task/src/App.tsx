c1d481c2cfc05a8abbaa799a0bd81c335fca1010
Rafał Chojnowski <chojnowskigr@gmail.com>
2021-02-10 23:54:12 +0100

    feat: error interceptor add

--- recruitment-task/src/App.tsx
+++ recruitment-task/src/App.tsx
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import styled from 'styled-components';
@@ -7,16 +7,35 @@ import { UsersList } from './components/users/UsersList';
 import { IconContext } from 'react-icons';
 import { PostDetails } from './components/posts/PostDeatils';
 import 'antd/dist/antd.css';
-
+import { onError } from '@apollo/client/link/error';
+import { openNotification } from './shared/Functions';
+import { FrownOutlined } from '@ant-design/icons';
 const AppContainer = styled.div`
 	width: 90%;
 	margin: 50px auto 0px auto;
 `;
 
 function App() {
+	const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+		if (graphQLErrors) {
+			openNotification(
+				'Error occured',
+				'Please check provided data',
+				5,
+				<FrownOutlined style={{ color: 'red' }} />
+			);
+		}
+
+		forward(operation);
+	});
+	const httpLink = new HttpLink({
+		uri: 'https://graphqlzero.almansi.me/api',
+	});
+
 	const client = new ApolloClient({
 		uri: 'https://graphqlzero.almansi.me/api',
 		cache: new InMemoryCache(),
+		link: from([errorLink, httpLink]),
 	});
 	return (
 		<ApolloProvider client={client}>
